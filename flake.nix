c2334a28880fafe5c8679246798a60ebafae5bdd
Chovy <veryverychovy@proton.me>
2026-06-09 13:38:15 +0000

    use the 26.05 channel

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Chovy's NixOS";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
